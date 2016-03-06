60787bcdeee7d68a1b153d72175f44f78d7e0dc4
Rob Rix <rob.rix@github.com>
2016-03-06 09:41:56 -0500

    Heading for the types.

--- src/Derivative/Parser.hs
+++ src/Derivative/Parser.hs
@@ -3,6 +3,8 @@ module Derivative.Parser where
 
 import Control.Applicative
 
+-- Types
+
 -- | A parser type encoding concatenation, alternation, repetition, &c. as first-order constructors.
 data Parser a where
   Cat :: Parser a -> Parser b -> Parser (a, b)
