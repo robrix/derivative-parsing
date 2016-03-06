c1b36c50a52284e8b49b485024a13de483a23729
Rob Rix <rob.rix@github.com>
2016-03-06 10:16:32 -0500

    Stub in a Lam type representing lambdas.

--- test/Derivative/Parser/Spec.hs
+++ test/Derivative/Parser/Spec.hs
@@ -6,3 +6,9 @@ spec :: Spec
 spec = do
   describe "fmap" $ do
     it "does a thing" $ True `shouldBe` True
+
+
+-- Types
+
+data Lam = Var String | Abs String Lam | App' Lam Lam
+  deriving (Eq, Show)
