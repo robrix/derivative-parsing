8778bedc8b98987ce5366e4db42765b41cd49467
Rob Rix <rob.rix@github.com>
2016-03-06 09:37:22 -0500

    Add map parsers.

--- src/Derivative/Parser.hs
+++ src/Derivative/Parser.hs
@@ -5,6 +5,7 @@ data Parser a where
   Cat :: Parser a -> Parser b -> Parser (a, b)
   Alt :: Parser a -> Parser b -> Parser (Either a b)
   Rep :: Parser a -> Parser [a]
+  Map :: Parser a -> (a -> b) -> Parser b
   Lit :: Char -> Parser Char
   Ret :: [a] -> Parser a
   Nul :: Parser a
