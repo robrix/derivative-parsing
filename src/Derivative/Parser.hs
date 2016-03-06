f2a657e365fcc6e13b10605f9af634f1fd111c85
Rob Rix <rob.rix@github.com>
2016-03-06 09:38:15 -0500

    Reorder the operands of Map.

--- src/Derivative/Parser.hs
+++ src/Derivative/Parser.hs
@@ -5,7 +5,7 @@ data Parser a where
   Cat :: Parser a -> Parser b -> Parser (a, b)
   Alt :: Parser a -> Parser b -> Parser (Either a b)
   Rep :: Parser a -> Parser [a]
-  Map :: Parser a -> (a -> b) -> Parser b
+  Map :: (a -> b) -> Parser a -> Parser b
   App :: Parser (a -> b) -> Parser a -> Parser b
   Bnd :: Parser a -> (a -> Parser b) -> Parser b
   Lit :: Char -> Parser Char
