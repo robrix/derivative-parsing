57968b045f599b354a5245a775de1902fe137f2b
Rob Rix <rob.rix@github.com>
2016-03-06 10:10:02 -0500

    The derivative distributes through binds.

--- src/Derivative/Parser.hs
+++ src/Derivative/Parser.hs
@@ -45,6 +45,7 @@ deriv (Alt a b) c = Alt (deriv a c) (deriv b c)
 deriv (Rep p) c = (:) <$> deriv p c <*> Rep p
 deriv (Map f p) c = Map f (deriv p c)
 deriv (App f p) c = App (deriv f c) p <|> App (Ret (parseNull f)) (deriv p c)
+deriv (Bnd p f) c = Bnd (deriv p c) f
 deriv (Lit c') c = if c == c' then Ret [c] else Nul
 deriv _ _ = Nul
 
