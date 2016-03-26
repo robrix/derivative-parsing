100b3205ebff64e8a4736331ec2359604cc69452
Rob Rix <rob.rix@github.com>
2016-03-25 22:20:11 -0400

    :fire: memoOn & supporting machinery.

--- src/Data/Memo.hs
+++ src/Data/Memo.hs
@@ -2,28 +2,13 @@
 module Data.Memo
 ( memo
 , memoFrom
-, memoOn
 , memoStable
 , memoStableFrom
 ) where
 
-import Control.Arrow
 import Data.IORef
 import System.IO.Unsafe
 import System.Mem.StableName
-
-applyPartial :: Eq key => IORef [(key, value)] -> Maybe value -> ((Maybe key, input) -> value) -> (Maybe key, input) -> value
-applyPartial ref from f arg | (Just key, _) <- arg = unsafePerformIO $ do
-                              table <- readIORef ref
-                              maybe (write ref key) return (key `lookup` table)
-                            | otherwise = f arg
-  where write ref key = do
-          _ <- case from of
-            Just from -> modifyIORef' ref (insert key from)
-            _ -> return ()
-          let result = f arg
-          result `seq` modifyIORef' ref (insert key result)
-          return $! result
 
 applyStable :: IORef [(StableName a, b)] -> Maybe b -> (a -> b) -> a -> b
 applyStable ref from f arg = unsafePerformIO $ do
@@ -71,14 +56,6 @@ memoFrom :: Eq a => b -> (a -> b) -> a -> b
 memoFrom from f = unsafePerformIO $ do
   ref <- newIORef []
   ref `seq` return $! apply ref (Just from) f
-
-memoOn :: Eq key => (input -> Maybe key) -> Maybe value -> (input -> value) -> input -> value
-memoOn on from = (. (on &&& id)) . memoPartial from . (. snd)
-
-memoPartial :: Eq key => Maybe value -> ((Maybe key, input) -> value) -> (Maybe key, input) -> value
-memoPartial from f = unsafePerformIO $ do
-  ref <- newIORef []
-  ref `seq` return $! applyPartial ref from f
 
 insert :: key -> value -> [(key, value)] -> [(key, value)]
 insert key value = ((key, value) :)
