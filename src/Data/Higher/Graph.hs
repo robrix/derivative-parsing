6557cb72a2e0b9fec0319f50ae3928ce29a06c73
Rob Rix <rob.rix@github.com>
2016-03-27 12:29:03 -0400

    Lift the variable type parameter.

--- src/Data/Higher/Graph.hs
+++ src/Data/Higher/Graph.hs
@@ -2,8 +2,8 @@
 module Data.Higher.Graph where
 
 data HRec h v a
-  = Var v
-  | Mu ([v] -> [h (HRec h v) a])
+  = Var (v a)
+  | Mu (forall a. [v a] -> [h (HRec h v) a])
   | In (h (HRec h v) a)
 
 newtype HGraph h a = Down { up :: forall v. HRec h v a }
