5c56caa0af19dff378e155a04169914447a1ccd8
Rob Rix <rob.rix@github.com>
2016-03-27 12:29:45 -0400

    Define hgfold over HGraphs wrapping HFunctors.

--- src/Data/Higher/Graph.hs
+++ src/Data/Higher/Graph.hs
@@ -1,5 +1,7 @@
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
 module Data.Higher.Graph where
+
+import Data.Higher.Functor
 
 data HRec h v a
   = Var (v a)
@@ -8,3 +10,9 @@ data HRec h v a
 
 newtype HGraph h a = HDown { hup :: forall v. HRec h v a }
 
+hgfold :: forall h v c. HFunctor h => (forall a. v a -> c a) -> (forall a. (forall a. [v a] -> [c a]) -> c a) -> (forall a. h c a -> c a) -> forall a. HGraph h a -> c a
+hgfold var bind recur = trans . hup
+  where trans :: HRec h v a -> c a
+        trans (Var x) = var x
+        trans (Mu g) = bind (map (recur . hfmap trans) . g)
+        trans (In fa) = recur (hfmap trans fa)
