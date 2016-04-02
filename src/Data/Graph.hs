f9fa891ede8b0940e162cdb9ace5cfb4f3ad8d16
Rob Rix <rob.rix@github.com>
2016-04-02 10:16:47 -0400

    Add the Graph type.

--- src/Data/Graph.hs
+++ src/Data/Graph.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE RankNTypes #-}
 module Data.Graph where
 
 data Rec f v
   = Var v
   | Mu ([v] -> [f (Rec f v)])
   | In (f (Rec f v))
+
+newtype Graph f = Down { up :: forall v. Rec f v }
