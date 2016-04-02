46474f1cd56fb67dd960b60f70f72009225005dc
Rob Rix <rob.rix@github.com>
2016-04-02 10:19:00 -0400

    Define the gfold operator for Graph.

--- src/Data/Graph.hs
+++ src/Data/Graph.hs
@@ -7,3 +7,9 @@ data Rec f v
   | In (f (Rec f v))
 
 newtype Graph f = Down { up :: forall v. Rec f v }
+
+gfold :: Functor f => (t -> c) -> (([t] -> [c]) -> c) -> (f c -> c) -> Graph f -> c
+gfold var bind recur = trans . up
+  where trans (Var v) = var v
+        trans (Mu g) = bind (map (recur . fmap trans) . g)
+        trans (In fa) = recur (fmap trans fa)
