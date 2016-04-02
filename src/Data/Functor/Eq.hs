5ac50896fcfd9f4fa9e0a20c473c94a13a81dbcc
Rob Rix <rob.rix@github.com>
2016-04-02 10:35:01 -0400

    Try an Eq instance for f a where EqF f, Eq a.

--- src/Data/Functor/Eq.hs
+++ src/Data/Functor/Eq.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Data.Functor.Eq where
 
 class Functor f => EqF f where
   eqF :: (r -> r -> Bool) -> f r -> f r -> Bool
+
+instance (EqF f, Eq a) => Eq (f a) where
+  (==) = eqF (==)
