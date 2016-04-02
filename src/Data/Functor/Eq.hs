dae73a7ecbb3a406a66822928ed933f8a536d28b
Rob Rix <rob.rix@github.com>
2016-04-02 10:37:00 -0400

    Revert "Try an Eq instance for f a where EqF f, Eq a."
    This reverts commit 5ac50896fcfd9f4fa9e0a20c473c94a13a81dbcc.

--- src/Data/Functor/Eq.hs
+++ src/Data/Functor/Eq.hs
@@ -1,8 +1,4 @@
-{-# LANGUAGE FlexibleInstances #-}
 module Data.Functor.Eq where
 
 class Functor f => EqF f where
   eqF :: (r -> r -> Bool) -> f r -> f r -> Bool
-
-instance (EqF f, Eq a) => Eq (f a) where
-  (==) = eqF (==)
