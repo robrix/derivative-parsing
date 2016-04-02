a58cdb06bd97184de586b3806529e703199d759f
Rob Rix <rob.rix@github.com>
2016-04-02 15:10:14 -0400

    Add an HEq instance for Const.

--- src/Data/Higher/Eq.hs
+++ src/Data/Higher/Eq.hs
@@ -1,4 +1,10 @@
 module Data.Higher.Eq where
 
+import Control.Applicative
+import Data.Function
+
 class HEq f where
   heq :: f a -> f a -> Bool
+
+instance Eq a => HEq (Const a)
+  where heq = (==) `on` getConst
