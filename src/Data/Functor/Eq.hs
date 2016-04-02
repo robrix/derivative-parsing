900084c67f21d47c5c1676bfdedc209acbbbec03
Rob Rix <rob.rix@github.com>
2016-04-02 14:33:04 -0400

    Add an EqF instance for [].

--- src/Data/Functor/Eq.hs
+++ src/Data/Functor/Eq.hs
@@ -2,3 +2,6 @@ module Data.Functor.Eq where
 
 class Functor f => EqF f where
   eqF :: (r -> r -> Bool) -> f r -> f r -> Bool
+
+instance EqF [] where
+  eqF eq a b = length a == length b && and (zipWith eq a b)
