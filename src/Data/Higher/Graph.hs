6cefffcc06f6b7aa86e693e15a77506e7eebb2d9
Rob Rix <rob.rix@github.com>
2016-04-02 15:17:19 -0400

    Move the definition of hgfold up.

--- src/Data/Higher/Graph.hs
+++ src/Data/Higher/Graph.hs
@@ -15,6 +15,13 @@ data HRec h v a
 
 newtype HGraph h a = HDown { hup :: forall v. HRec h v a }
 
+hgfold :: forall f v c. HFunctor f => (v ~> c) -> (forall a. ([v a] -> [c a]) -> c a) -> (f c ~> c) -> HGraph f ~> c
+hgfold var bind recur = trans . hup
+  where trans :: forall a. HRec f v a -> c a
+        trans (Var x) = var x
+        trans (Mu g) = bind (map (recur . hfmap trans) . g)
+        trans (In fa) = recur (hfmap trans fa)
+
 gfold :: forall h v c a. HFunctor h => (forall a. v a -> c) -> (forall a. ([v a] -> [c]) -> c) -> (forall a. h (Const c) a -> c) -> HGraph h a -> c
 gfold var bind recur = getConst . trans . hup
   where trans :: HRec h v ~> Const c
@@ -44,10 +51,3 @@ fhfixVal :: (EqF f, HEq h) => f (h a) -> (f (h a) -> f (h a)) -> f (h a)
 fhfixVal v f = if v `eq` v' then v else fhfixVal v' f
   where v' = f v
         eq = eqF heq
-
-hgfold :: forall f v c. HFunctor f => (v ~> c) -> (forall a. ([v a] -> [c a]) -> c a) -> (f c ~> c) -> HGraph f ~> c
-hgfold var bind recur = trans . hup
-  where trans :: forall a. HRec f v a -> c a
-        trans (Var x) = var x
-        trans (Mu g) = bind (map (recur . hfmap trans) . g)
-        trans (In fa) = recur (hfmap trans fa)
