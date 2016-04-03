a67b1cdd317a6cc7a956352e0450e11ddd31be87
Rob Rix <rob.rix@github.com>
2016-04-03 10:01:32 -0400

    Add an EqF instance for Const.

--- src/Data/Functor/Eq.hs
+++ src/Data/Functor/Eq.hs
@@ -1,7 +1,12 @@
 module Data.Functor.Eq where
 
+import Control.Applicative
+
 class Functor f => EqF f where
   eqF :: (r -> r -> Bool) -> f r -> f r -> Bool
 
 instance EqF [] where
   eqF eq a b = and (zipWith eq a b) && length a == length b
+
+instance Eq a => EqF (Const a)
+  where eqF _ a b = getConst a == getConst b
