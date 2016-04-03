681f488b57738d969589f20d89c6d5b8a551fe06
Rob Rix <rob.rix@github.com>
2016-04-02 23:14:27 -0400

    Test element equality first.
    This will terminate if either list is finite.

--- src/Data/Functor/Eq.hs
+++ src/Data/Functor/Eq.hs
@@ -4,4 +4,4 @@ class Functor f => EqF f where
   eqF :: (r -> r -> Bool) -> f r -> f r -> Bool
 
 instance EqF [] where
-  eqF eq a b = length a == length b && and (zipWith eq a b)
+  eqF eq a b = and (zipWith eq a b) && length a == length b
