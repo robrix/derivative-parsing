dc337afb42adfe1888210f10f89b03dfad7ed026
Rob Rix <rob.rix@github.com>
2016-04-02 23:56:20 -0400

    Add a HBifunctor typeclass with an hbimap method.

--- src/Data/Higher/Bifunctor.hs
+++ src/Data/Higher/Bifunctor.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Data.Higher.Bifunctor where
+
+import Data.Higher.Transformation
+
+class HBifunctor h where
+  hbimap :: (a ~> b) -> (c ~> d) -> h a c ~> h b d
