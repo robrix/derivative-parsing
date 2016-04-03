aa653497fbd9697c7c418b64e8247411a3f81123
Rob Rix <rob.rix@github.com>
2016-04-03 00:21:57 -0400

    Define an HEqF typeclass.

--- src/Data/Higher/Functor/Eq.hs
+++ src/Data/Higher/Functor/Eq.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE RankNTypes #-}
 module Data.Higher.Functor.Eq where
+
+import Data.Higher.Functor
+
+class HFunctor f => HEqF f
+  where heqF :: (forall a. r a -> r a -> Bool) -> f r a -> f r a -> Bool
