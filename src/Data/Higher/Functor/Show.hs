866ce943d3d8d53ee8a230f12ab4b8788f6ccf7a
Rob Rix <rob.rix@github.com>
2016-04-03 10:03:53 -0400

    Add an HShowF typeclass.

--- src/Data/Higher/Functor/Show.hs
+++ src/Data/Higher/Functor/Show.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE RankNTypes #-}
 module Data.Higher.Functor.Show where
+
+import Data.Higher.Functor
+
+class HFunctor f => HShowF f
+  where hshowF :: (forall a. r a -> String) -> f r a -> String
