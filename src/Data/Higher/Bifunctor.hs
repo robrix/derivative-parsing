966e6d7122c468ac78153ed7892f8ce803df4a97
Rob Rix <rob.rix@github.com>
2016-04-09 13:29:12 -0400

    Add an HBifunctor1 class for higher-order bifunctors whose operands are first-order and second-order respectively.

--- src/Data/Higher/Bifunctor.hs
+++ src/Data/Higher/Bifunctor.hs
@@ -11,3 +11,12 @@ class HBifunctor h where
 
   hsecond :: (c ~> d) -> h a c ~> h a d
   hsecond = hbimap id
+
+class HBifunctor1 h where
+  hbimap1 :: (a ~> b) -> (c -> d) -> h a c ~> h b d
+
+  hfirst1 :: (a ~> b) -> h a c ~> h b c
+  hfirst1 f = hbimap1 f id
+
+  hsecond1 :: (c -> d) -> h a c ~> h a d
+  hsecond1 = hbimap1 id
