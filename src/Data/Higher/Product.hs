ab71948cce2537498c618b5ea2732677d768a4ad
Rob Rix <rob.rix@github.com>
2016-04-17 09:43:15 -0400

    Add a *** implementation over higher products.

--- src/Data/Higher/Product.hs
+++ src/Data/Higher/Product.hs
@@ -1,6 +1,14 @@
 {-# LANGUAGE RankNTypes, TypeOperators #-}
 module Data.Higher.Product where
 
+import Data.Higher.Transformation
+
 infixr :*:
 
 data (:*:) f g a = f a :*: g a
+
+
+infixr 3 ***
+
+(***) :: (f ~> f') -> (g ~> g') -> (f :*: g) ~> (f' :*: g')
+f *** g = \ (a :*: b) -> f a :*: g b
