64a1bd0a0e26000e51f4ddc6bafd88814a1cf241
Rob Rix <rob.rix@github.com>
2016-04-19 09:47:25 -0400

    Rename the HIsofunctor type parameters.

--- src/Data/Higher/Isofunctor.hs
+++ src/Data/Higher/Isofunctor.hs
@@ -4,4 +4,4 @@ module Data.Higher.Isofunctor where
 import Data.Higher.Transformation
 
 class HIsofunctor f
-  where hisomap :: (a ~> b) -> (b ~> a) -> f a ~> f b
+  where hisomap :: (c ~> d) -> (d ~> c) -> f c ~> f d
