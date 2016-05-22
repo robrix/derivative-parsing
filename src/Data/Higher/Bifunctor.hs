2fe11ff5aa88116bc30cf371a9782972b6ff4507
Rob Rix <rob.rix@github.com>
2016-05-21 23:24:38 -0400

    Declare and implement hfirst and hsecond.

--- src/Data/Higher/Bifunctor.hs
+++ src/Data/Higher/Bifunctor.hs
@@ -5,3 +5,9 @@ import Data.Higher.Transformation
 
 class HBifunctor p
   where hbimap :: (f ~> f') -> (g ~> g') -> p f g ~> p f' g'
+
+        hfirst :: (f ~> f') -> p f g ~> p f' g
+        hfirst = (`hbimap` id)
+
+        hsecond :: (g ~> g') -> p f g ~> p f g'
+        hsecond = (id `hbimap`)
