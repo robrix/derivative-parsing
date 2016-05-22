45022700da689a0c777d50c7bc227e3931555d53
Rob Rix <rob.rix@github.com>
2016-05-21 23:24:27 -0400

    Add the HBifunctor typeclass.

--- src/Data/Higher/Bifunctor.hs
+++ src/Data/Higher/Bifunctor.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Data.Higher.Bifunctor where
+
+import Data.Higher.Transformation
+
+class HBifunctor p
+  where hbimap :: (f ~> f') -> (g ~> g') -> p f g ~> p f' g'
