5972b266b4758b63b5892cf2fbec250f48044c4f
Rob Rix <rob.rix@github.com>
2016-05-29 11:42:26 -0400

    Remove an unnecessary explicit forall.

--- src/Data/Higher/Graph.hs
+++ src/Data/Higher/Graph.hs
@@ -90,7 +90,7 @@ hgcata f rec = case rec of
 
 -- Maps
 
-transform :: forall f g. HFunctor f => (forall v. f (HRec g v) ~> g (HRec g v)) -> HGraph f ~> HGraph g
+transform :: HFunctor f => (forall v. f (HRec g v) ~> g (HRec g v)) -> HGraph f ~> HGraph g
 transform f = modifyGraph (hmap f)
 
 hmap :: HFunctor f => (f (HRec g v) ~> g (HRec g v)) -> HRec f v ~> HRec g v
