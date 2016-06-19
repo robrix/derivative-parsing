0ebf0645b9beb0668f2a7abddea1793d78aa4abf
Rob Rix <rob.rix@github.com>
2016-06-19 16:37:59 -0400

    :fire: Data.Higher.Isofunctor.

--- src/Data/Higher/Isofunctor.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE RankNTypes, TypeOperators #-}
-module Data.Higher.Isofunctor where
-
-import Data.Higher.Transformation
-
-class HIsofunctor f
-  where hisomap :: (c ~> d) -> (d ~> c) -> (f c a -> f d a, f d a -> f c a)
