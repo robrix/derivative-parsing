bb03252311d862fc20af80c25dd474ea5aee6ce1
Rob Rix <rob.rix@github.com>
2016-06-19 16:52:20 -0400

    :fire: Data.Higher.Function.

--- src/Data/Higher/Function.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE RankNTypes, ScopedTypeVariables, TypeOperators #-}
-module Data.Higher.Function where
-
-import Data.Higher.Transformation
-
-hfix :: forall f g. ((f ~> g) -> f ~> g) -> f ~> g
-hfix f = x
-  where x :: f ~> g
-        x = f x
