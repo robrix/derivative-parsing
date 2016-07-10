ae61c385115b095dc09b2121d62d56917693f5be
Rob Rix <rob.rix@github.com>
2016-07-10 14:25:36 -0400

    Allow hbind to vary the type further.

--- src/Control/Higher/Monad.hs
+++ src/Control/Higher/Monad.hs
@@ -2,7 +2,6 @@
 module Control.Higher.Monad where
 
 import Control.Higher.Applicative
-import Data.Higher.Transformation
 
 class HApplicative m => HMonad m where
-  hbind :: m a z -> (a ~> m b) -> m b z
+  hbind :: m a p -> (a p -> m b q) -> m b q
