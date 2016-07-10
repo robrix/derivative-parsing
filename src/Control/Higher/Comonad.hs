11afa7a65407ed81730d46de2c0da58f0ba974f3
Rob Rix <rob.rix@github.com>
2016-07-10 15:52:40 -0400

    Stub in an extend method.

--- src/Control/Higher/Comonad.hs
+++ src/Control/Higher/Comonad.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Control.Higher.Comonad where
 
 import Data.Higher.Transformation
 
 class HComonad w where
   hextract :: w a ~> a
+
+  hextend :: (w a ~> b) -> w a ~> w b
