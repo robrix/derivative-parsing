add0606512081391f5185c13c394ad3728a9467e
Rob Rix <rob.rix@github.com>
2016-07-10 16:24:28 -0400

    Implement an annotating gcata.

--- src/Data/Higher/Functor/Foldable.hs
+++ src/Data/Higher/Functor/Foldable.hs
@@ -50,6 +50,11 @@ hylo f g = go
 newtype Fix f a = Fix { unFix :: f (Fix f) a }
 
 
+agcata :: forall t w a. (Recursive t, HComonad w) => (forall b. Base t (w b) ~> w (Base t b)) -> (Base t (w a) ~> a) -> t ~> w a
+agcata dist alg = go
+  where go :: t ~> w a
+        go = hfmap alg . dist . hfmap (hduplicate . go) . project
+
 gcata :: forall t w a. (Recursive t, HComonad w) => (forall b. Base t (w b) ~> w (Base t b)) -> (Base t (w a) ~> a) -> t ~> a
 gcata dist alg = alg . hextract . go
   where go :: t ~> w (Base t (w a))
