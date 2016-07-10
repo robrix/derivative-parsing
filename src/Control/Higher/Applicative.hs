429b37ab8b2f2f7b386b8a6f7a68c15d25ba4e86
Rob Rix <rob.rix@github.com>
2016-07-10 13:59:14 -0400

    Define left/right-dropping sequencing methods.

--- src/Control/Higher/Applicative.hs
+++ src/Control/Higher/Applicative.hs
@@ -9,6 +9,12 @@ class HFunctor f => HApplicative (f :: (k -> *) -> k -> *) where
 
   (<:*:>) :: f (a ~~> b) z -> f a z -> f b z
 
+  (*:>) :: f b a -> f c a -> f c a
+  a *:> b = (const (A id) `hfmap` a) <:*:> b
+
+  (<:*) :: f c a -> f b a -> f c a
+  (<:*) = hliftA2 const
+
 
 hliftA2 :: HApplicative f => (forall z. a z -> b z -> c z) -> f a z -> f b z -> f c z
 hliftA2 f a b = hfmap (A . f) a <:*:> b
