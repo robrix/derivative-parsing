36b703b536f317982b070cb5471267798718b801
Rob Rix <rob.rix@github.com>
2016-07-09 21:41:53 -0400

    Correct HBifunctor to involve a natural transformation.

--- src/Data/Higher/Bifunctor.hs
+++ src/Data/Higher/Bifunctor.hs
@@ -4,10 +4,10 @@ module Data.Higher.Bifunctor where
 import Data.Higher.Transformation
 
 class HBifunctor p where
-  hbimap :: (a ~> c) -> (b ~> d) -> p a b -> p c d
+  hbimap :: (a ~> c) -> (b ~> d) -> p a b ~> p c d
 
-  hfirst :: (a ~> c) -> p a b -> p c b
+  hfirst :: (a ~> c) -> p a b ~> p c b
   hfirst = (`hbimap` id)
 
-  hsecond :: (b ~> d) -> p a b -> p a d
+  hsecond :: (b ~> d) -> p a b ~> p a d
   hsecond = hbimap id
