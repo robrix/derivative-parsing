bafbf7826f140529267d426e781a5b8a8ad26cd5
Rob Rix <rob.rix@github.com>
2016-07-09 21:04:59 -0400

    Add Cofree & CofreeF types to Control.Higher.Comonad.Cofree.

--- src/Control/Higher/Comonad/Cofree.hs
+++ src/Control/Higher/Comonad/Cofree.hs
@@ -1,1 +1,6 @@
+{-# LANGUAGE PolyKinds #-}
 module Control.Higher.Comonad.Cofree where
+
+data CofreeF f v b a = v a :< f b a
+
+newtype Cofree f v a = Cofree { runCofree :: CofreeF f v (Cofree f v) a }
