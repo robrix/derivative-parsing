28cc510c62fffd2745a4ed136595b3cfa29dcc21
Rob Rix <rob.rix@github.com>
2016-07-10 10:12:29 -0400

    Higher sums are bifunctors.

--- src/Data/Higher/Sum.hs
+++ src/Data/Higher/Sum.hs
@@ -1,7 +1,16 @@
 {-# LANGUAGE PolyKinds, TypeOperators #-}
 module Data.Higher.Sum where
 
+import Data.Higher.Bifunctor
+
 data (f :+: g) a
   = L (f a)
   | R (g a)
 
+
+-- Instances
+
+instance HBifunctor (:+:) where
+  hbimap f g s = case s of
+    L l -> L (f l)
+    R r -> R (g r)
