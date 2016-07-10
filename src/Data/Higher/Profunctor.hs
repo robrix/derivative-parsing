5e55b412a99bfc936e5bd0111842ef16b76e87d9
Rob Rix <rob.rix@github.com>
2016-07-10 11:36:05 -0400

    Define hlmap/hrmap functions over profunctors.

--- src/Data/Higher/Profunctor.hs
+++ src/Data/Higher/Profunctor.hs
@@ -5,3 +5,9 @@ import Data.Higher.Transformation
 
 class HProfunctor p where
   hdimap :: (a ~> b) -> (c ~> d) -> p b c ~> p a d
+
+  hlmap :: (a ~> b) -> p b c ~> p a c
+  hlmap = (`hdimap` id)
+
+  hrmap :: (c ~> d) -> p b c ~> p b d
+  hrmap = hdimap id
