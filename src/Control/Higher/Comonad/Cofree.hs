77ba3bef5e70cbfccfff5deef7c52e1e33f01147
Rob Rix <rob.rix@github.com>
2016-07-09 21:42:57 -0400

    Add an HBifunctor instance over CofreeF.

--- src/Control/Higher/Comonad/Cofree.hs
+++ src/Control/Higher/Comonad/Cofree.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE PolyKinds, TypeFamilies #-}
 module Control.Higher.Comonad.Cofree where
 
+import Data.Higher.Bifunctor
+import Data.Higher.Functor
 import Data.Higher.Functor.Foldable
 
 data CofreeF f v b a = v a :< f b a
@@ -9,5 +11,9 @@ newtype Cofree f v a = Cofree { runCofree :: CofreeF f v (Cofree f v) a }
 
 
 -- Instances
+
+instance HFunctor f => HBifunctor (CofreeF f) where
+  hbimap f g (va :< fba) = f va :< hfmap g fba
+
 
 type instance Base (Cofree f v) = CofreeF f v
