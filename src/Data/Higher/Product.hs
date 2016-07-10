676cff9361f5ef86e88473f12e06fec43c1f2455
Rob Rix <rob.rix@github.com>
2016-07-10 10:04:16 -0400

    Products are bifunctors.

--- src/Data/Higher/Product.hs
+++ src/Data/Higher/Product.hs
@@ -1,4 +1,12 @@
 {-# LANGUAGE TypeOperators #-}
 module Data.Higher.Product where
 
+import Data.Higher.Bifunctor
+
 data (f :*: g) a = (:*:) { hfst :: f a, hsnd :: g a }
+
+
+-- Instances
+
+instance HBifunctor (:*:) where
+  hbimap f g p = f (hfst p) :*: g (hsnd p)
