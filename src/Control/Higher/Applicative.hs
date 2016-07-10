b1a07355193ea8325e1b07d6029fb0e1195abe72
Rob Rix <rob.rix@github.com>
2016-07-10 13:57:14 -0400

    Define HApplicative with a pure analogue.

--- src/Control/Higher/Applicative.hs
+++ src/Control/Higher/Applicative.hs
@@ -1,2 +1,8 @@
+{-# LANGUAGE PolyKinds, RankNTypes, TypeOperators #-}
 module Control.Higher.Applicative where
 
+import Data.Higher.Functor
+import Data.Higher.Transformation
+
+class HFunctor f => HApplicative (f :: (k -> *) -> k -> *) where
+  hpure :: a ~> f a
