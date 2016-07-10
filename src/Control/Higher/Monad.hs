3413a5a0976ccc203a7ee8464b1a8e9d8b730b34
Rob Rix <rob.rix@github.com>
2016-07-10 14:06:04 -0400

    Add a hbind method.

--- src/Control/Higher/Monad.hs
+++ src/Control/Higher/Monad.hs
@@ -1,1 +1,8 @@
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Control.Higher.Monad where
+
+import Control.Higher.Applicative
+import Data.Higher.Transformation
+
+class HApplicative m => HMonad m where
+  hbind :: m a z -> (a ~> m b) -> m b z
