7729084a27b3499729d8416bb0e740079cb35983
Rob Rix <rob.rix@github.com>
2016-07-10 13:59:04 -0400

    Define a higher liftA2 analogue.

--- src/Control/Higher/Applicative.hs
+++ src/Control/Higher/Applicative.hs
@@ -8,3 +8,7 @@ class HFunctor f => HApplicative (f :: (k -> *) -> k -> *) where
   hpure :: a ~> f a
 
   (<:*:>) :: f (a ~~> b) z -> f a z -> f b z
+
+
+hliftA2 :: HApplicative f => (forall z. a z -> b z -> c z) -> f a z -> f b z -> f c z
+hliftA2 f a b = hfmap (A . f) a <:*:> b
