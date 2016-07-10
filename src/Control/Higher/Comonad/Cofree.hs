11791a5b43d72b48b9bb4ce63941fb0de2b9bd29
Rob Rix <rob.rix@github.com>
2016-07-09 21:47:17 -0400

    Add an HFunctor instance over Cofree.

--- src/Control/Higher/Comonad/Cofree.hs
+++ src/Control/Higher/Comonad/Cofree.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE PolyKinds, TypeFamilies #-}
+{-# LANGUAGE InstanceSigs, RankNTypes, PolyKinds, ScopedTypeVariables, TypeFamilies, TypeOperators #-}
 module Control.Higher.Comonad.Cofree where
 
 import Data.Higher.Bifunctor
 import Data.Higher.Functor
 import Data.Higher.Functor.Foldable
+import Data.Higher.Transformation
 
 data CofreeF f v b a = v a :< f b a
 
@@ -17,6 +18,12 @@ cofree = Cofree
 
 instance HFunctor f => HBifunctor (CofreeF f) where
   hbimap f g (va :< fba) = f va :< hfmap g fba
+
+instance HFunctor f => HFunctor (Cofree f) where
+  hfmap :: forall a b. (a ~> b) -> Cofree f a ~> Cofree f b
+  hfmap f = go
+    where go :: Cofree f a ~> Cofree f b
+          go = cofree . hbimap f go . runCofree
 
 
 type instance Base (Cofree f v) = CofreeF f v
