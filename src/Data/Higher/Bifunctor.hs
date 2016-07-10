0c31d1b864a2c880cec46fb01aee1e7f18b1be37
Rob Rix <rob.rix@github.com>
2016-07-09 21:26:30 -0400

    Add hfirst and hsecond methods to Bifunctor.

--- src/Data/Higher/Bifunctor.hs
+++ src/Data/Higher/Bifunctor.hs
@@ -5,3 +5,9 @@ import Data.Higher.Transformation
 
 class Bifunctor p where
   hbimap :: (a ~> c) -> (b ~> d) -> p a b -> p c d
+
+  hfirst :: (a ~> c) -> p a b -> p c b
+  hfirst = (`hbimap` id)
+
+  hsecond :: (b ~> d) -> p a b -> p a d
+  hsecond = hbimap id
