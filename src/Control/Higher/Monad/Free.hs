884e35eae231c0cc0301427b7d51d3260a1f9bb1
Rob Rix <rob.rix@github.com>
2016-07-10 11:52:13 -0400

    Define an HFunctor instance over Free.

--- src/Control/Higher/Monad/Free.hs
+++ src/Control/Higher/Monad/Free.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE InstanceSigs, PolyKinds, RankNTypes, ScopedTypeVariables, TypeOperators #-}
 module Control.Higher.Monad.Free where
+
+import Data.Higher.Functor
+import Data.Higher.Transformation
 
 data FreeF f v b a
   = Impure (f b a)
@@ -10,3 +13,14 @@ newtype Free f v a = Free { runFree :: FreeF f v (Free f v) a }
 
 wrap :: f (Free f v) a -> Free f v a
 wrap = Free . Impure
+
+
+-- Instances
+
+instance HFunctor f => HFunctor (Free f) where
+  hfmap :: forall a b. (a ~> b) -> Free f a ~> Free f b
+  hfmap f = go
+    where go :: Free f a ~> Free f b
+          go r = Free $ case runFree r of
+            Pure a -> Pure (f a)
+            Impure r -> Impure (hfmap go r)
