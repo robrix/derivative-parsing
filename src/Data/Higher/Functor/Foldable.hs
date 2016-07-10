14c97e4ed2d5f5c62ee8a8f1204a273645851691
Rob Rix <rob.rix@github.com>
2016-07-09 21:16:40 -0400

    Stub in a basic higher Recursive typeclass.

--- src/Data/Higher/Functor/Foldable.hs
+++ src/Data/Higher/Functor/Foldable.hs
@@ -1,1 +1,15 @@
+{-# LANGUAGE FlexibleContexts, PolyKinds, RankNTypes, ScopedTypeVariables, TypeFamilies, TypeOperators #-}
 module Data.Higher.Functor.Foldable where
+
+import Data.Higher.Functor
+import Data.Higher.Transformation
+
+type family Base (t :: j) :: (k -> *) -> k -> *
+
+class HFunctor (Base t) => Recursive t where
+  project :: t a -> Base t t a
+
+  cata :: forall c. (Base t c ~> c) -> t ~> c
+  cata f = go
+    where go :: t ~> c
+          go = f . hfmap go . project
