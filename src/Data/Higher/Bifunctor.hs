30d6eb8dacab44b3d16fb4b68cab3da438ad7ec2
Rob Rix <rob.rix@github.com>
2016-07-09 21:25:13 -0400

    Add a Bifunctor typeclass.

--- src/Data/Higher/Bifunctor.hs
+++ src/Data/Higher/Bifunctor.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Data.Higher.Bifunctor where
+
+import Data.Higher.Transformation
+
+class Bifunctor p where
+  hbimap :: (a ~> c) -> (b ~> d) -> p a b -> p c d
