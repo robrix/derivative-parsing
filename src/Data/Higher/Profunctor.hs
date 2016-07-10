9796dcc850ff7956154f293bc9f6a64163bd3864
Rob Rix <rob.rix@github.com>
2016-07-10 11:34:22 -0400

    Define higher profunctors.

--- src/Data/Higher/Profunctor.hs
+++ src/Data/Higher/Profunctor.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Data.Higher.Profunctor where
+
+import Data.Higher.Transformation
+
+class HProfunctor p where
+  hdimap :: (a ~> b) -> (c ~> d) -> p b c ~> p a d
