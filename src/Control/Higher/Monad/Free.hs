abc54eb5fd4c335d6f8e169b215c894d57e58791
Rob Rix <rob.rix@github.com>
2016-07-10 11:50:40 -0400

    Correct the application of the functor.

--- src/Control/Higher/Monad/Free.hs
+++ src/Control/Higher/Monad/Free.hs
@@ -2,11 +2,11 @@
 module Control.Higher.Monad.Free where
 
 data FreeF f v b a
-  = Impure (f v b a)
+  = Impure (f b a)
   | Pure (v a)
 
 newtype Free f v a = Free { runFree :: FreeF f v (Free f v) a }
 
 
-wrap :: f v (Free f v) a -> Free f v a
+wrap :: f (Free f v) a -> Free f v a
 wrap = Free . Impure
