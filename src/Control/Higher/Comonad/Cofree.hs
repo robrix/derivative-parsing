69c22bc04f760acb9d8036a99d60d83a1cf627e8
Rob Rix <rob.rix@github.com>
2016-07-09 21:21:54 -0400

    Add an instance of Base over Cofree.

--- src/Control/Higher/Comonad/Cofree.hs
+++ src/Control/Higher/Comonad/Cofree.hs
@@ -1,6 +1,13 @@
-{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE PolyKinds, TypeFamilies #-}
 module Control.Higher.Comonad.Cofree where
+
+import Data.Higher.Functor.Foldable
 
 data CofreeF f v b a = v a :< f b a
 
 newtype Cofree f v a = Cofree { runCofree :: CofreeF f v (Cofree f v) a }
+
+
+-- Instances
+
+type instance Base (Cofree f v) = CofreeF f v
