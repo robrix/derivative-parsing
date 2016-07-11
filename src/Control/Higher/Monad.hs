cd63b547918526d4f322fbd9396bfa7ba648bc4b
Rob Rix <rob.rix@github.com>
2016-07-11 01:27:25 -0400

    Define a higher Bind typeclass (Monad sans Pointed).

--- src/Control/Higher/Monad.hs
+++ src/Control/Higher/Monad.hs
@@ -2,6 +2,9 @@
 module Control.Higher.Monad where
 
 import Control.Higher.Applicative
+import Data.Higher.Pointed
 
-class HApplicative m => HMonad m where
+class (HBind m, HPointed m) => HMonad m
+
+class HApply m => HBind m where
   hbind :: m a p -> (a p -> m b q) -> m b q
