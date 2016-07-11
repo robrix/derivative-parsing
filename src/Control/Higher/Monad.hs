81ea6e688acde09f0b080c4897527e304b39c2e6
Rob Rix <rob.rix@github.com>
2016-07-11 01:28:24 -0400

    Higher monads employ a natural transformation.

--- src/Control/Higher/Monad.hs
+++ src/Control/Higher/Monad.hs
@@ -3,8 +3,9 @@ module Control.Higher.Monad where
 
 import Control.Higher.Applicative
 import Data.Higher.Pointed
+import Data.Higher.Transformation
 
 class (HBind m, HPointed m) => HMonad m
 
 class HApply m => HBind m where
-  hbind :: m a p -> (a p -> m b q) -> m b q
+  hbind :: m a p -> (a ~> m b) -> m b p
