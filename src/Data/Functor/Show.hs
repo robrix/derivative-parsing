6e60182a135b0fab6878b3d73fcc62e1e7ce5b29
Rob Rix <rob.rix@github.com>
2016-07-17 10:40:43 -0400

    Implement ShowF over Const.

--- src/Data/Functor/Show.hs
+++ src/Data/Functor/Show.hs
@@ -1,4 +1,9 @@
 module Data.Functor.Show where
 
+import Data.Functor.Const
+
 class ShowF f
   where showsPrecF :: Int -> (Int -> a -> ShowS) -> f a -> ShowS
+
+instance Show a => ShowF (Const a)
+  where showsPrecF n _ = showsPrec n . getConst
