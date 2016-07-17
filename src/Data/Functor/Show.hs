5bb980677d43b4b3cbb587f8e5c51bc06e3e0e15
Rob Rix <rob.rix@github.com>
2016-07-17 12:14:01 -0400

    flip ShowF.

--- src/Data/Functor/Show.hs
+++ src/Data/Functor/Show.hs
@@ -3,7 +3,7 @@ module Data.Functor.Show (ShowF(..)) where
 import Data.Functor.Const
 
 class ShowF f
-  where showsPrecF :: Int -> (Int -> a -> ShowS) -> f a -> ShowS
+  where showsPrecF :: (Int -> a -> ShowS) -> Int -> f a -> ShowS
 
 
 -- | A wrapper to enable use of an extant 'Show' instance when defining a 'ShowF' instance.
@@ -13,10 +13,10 @@ data Showable a = Showable !(Int -> a -> ShowS) !a
 -- Instances
 
 instance Show a => ShowF (Const a)
-  where showsPrecF n _ = showsPrec n . getConst
+  where showsPrecF _ n = showsPrec n . getConst
 
 instance ShowF []
-  where showsPrecF n f = showsPrec n . fmap (Showable f)
+  where showsPrecF f n = showsPrec n . fmap (Showable f)
 
 instance Show (Showable a)
   where showsPrec n (Showable f a) = f n a
