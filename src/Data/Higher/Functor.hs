3e71d61f1b6a32b9e1aed4144048ff89d82a315c
Rob Rix <rob.rix@github.com>
2016-07-20 16:56:18 -0600

    :fire: unused HFunctor definitions.

--- src/Data/Higher/Functor.hs
+++ src/Data/Higher/Functor.hs
@@ -6,10 +6,3 @@ import Data.Higher.Transformation
 class HFunctor f where
   infixl 4 `hfmap`
   hfmap :: (a ~> b) -> f a ~> f b
-
-  (<:$) :: (forall z. a z) -> f b ~> f a
-  (<:$) z = hfmap (const z)
-
-infixl 4 <:$:>
-(<:$:>) :: HFunctor f => (a ~> b) -> f a ~> f b
-(<:$:>) = hfmap
