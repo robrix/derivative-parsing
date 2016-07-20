a92211cb51813e3c9047aca703ecc6d5f5eff98d
Rob Rix <rob.rix@github.com>
2016-07-20 17:00:59 -0400

    Add a hfoldr method with a default implementation.

--- src/Data/Higher/Foldable.hs
+++ src/Data/Higher/Foldable.hs
@@ -9,8 +9,17 @@ import Data.Monoid
 class HFoldable f a where
   hfoldMap :: HMonoid m => (a ~> m) -> f a ~> m
 
+  hfoldr :: (forall z. a z -> b z -> b z) -> forall z. b z -> f a z -> b z
+  hfoldr f z t = appHEndo (hfoldMap (HEndo . f) t) z
+
   hfold :: HMonoid a => f a ~> a
   hfold = hfoldMap id
 
   hlength :: f a z -> Int
   hlength = getSum . getConst . hfoldMap (const (Const (Sum 1)))
+
+newtype HEndo a z = HEndo { appHEndo :: a z -> a z }
+
+instance HMonoid (HEndo a) where
+  hempty = HEndo id
+  HEndo a `happend` HEndo b = HEndo (a . b)
