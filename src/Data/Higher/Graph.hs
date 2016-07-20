93abb1925758f860d88a76d0c999fc001b353ed9
Rob Rix <rob.rix@github.com>
2016-07-20 15:57:52 -0400

    Define a showsRecF function for RecF parameterized by Const Char.

--- src/Data/Higher/Graph.hs
+++ src/Data/Higher/Graph.hs
@@ -168,6 +168,13 @@ showsRec s n rec = case runFree rec of
                        . hshowsPrecF (showsRec (fmap (Const . getConst) s')) n (g a) . showString "\n)\n"
   Impure (In fa) -> hshowsPrecF (showsRec s) n fa
 
+showsRecF :: HShowF f => (forall b. [Const Char b]) -> Int -> RecF f (Const Char) (Const Char) a -> ShowS
+showsRecF s n rec = case rec of
+  Mu g -> let (a, s') = (head s, tail s) in
+              showString "Mu (\\ " . showChar (getConst a) . showString " ->\n  "
+              . hshowsPrecF showsPrec n (g a) . showString "\n)\n"
+  In fa -> hshowsPrecF showsPrec n fa
+
 
 -- Implementation details
 
