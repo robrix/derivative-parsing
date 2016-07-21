042793be7ec6a7e42f3acee4baf9385b94e6b44a
Rob Rix <rob.rix@github.com>
2016-07-21 10:13:29 -0400

    :fire: the HMonoid instance over Const.

--- src/Data/Higher/Monoid.hs
+++ src/Data/Higher/Monoid.hs
@@ -1,15 +1,9 @@
 module Data.Higher.Monoid where
 
-import Data.Functor.Const
-
 class HMonoid a where
   hempty :: a z
 
   happend :: a z -> a z -> a z
-
-instance Monoid a => HMonoid (Const a)
-  where hempty = Const mempty
-        Const a `happend` Const b = Const (a `mappend` b)
 
 instance HMonoid []
   where hempty = []
