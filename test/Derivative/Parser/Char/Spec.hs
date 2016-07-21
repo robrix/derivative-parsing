c1fccc45ddfa529240cb48bfc5f928499b71dc7a
Rob Rix <rob.rix@github.com>
2016-07-21 13:21:07 -0400

    Test that alphaNum agrees with isAlphaNum.

--- test/Derivative/Parser/Char/Spec.hs
+++ test/Derivative/Parser/Char/Spec.hs
@@ -1,6 +1,13 @@
 module Derivative.Parser.Char.Spec where
 
+import Data.Char
+import Derivative.Parser
+import Derivative.Parser.Char
 import Test.Hspec
+import Test.Hspec.QuickCheck
 
 spec :: Spec
-spec = pure ()
+spec = do
+  describe "alphaNum" $
+    prop "parses isAlphaNum characters" $
+      \ c -> null (parser alphaNum `parse` [c]) `shouldBe` not (isAlphaNum c)
