04255ad4c29e528e6464cf48c2e5395f422cf027
Rob Rix <rob.rix@github.com>
2016-07-22 01:00:10 -0400

    Benchmark repetitions at four different input lengths.

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+import Control.Applicative
+import Criterion.Main
+import Derivative.Parser
+
 main :: IO ()
-main = print "TBD"
+main = defaultMain
+  [ bench "repetition/1" $ whnf (parse (parser (many (char 'a')))) (replicate 1 'a')
+  , bench "repetition/10" $ whnf (parse (parser (many (char 'a')))) (replicate 10 'a')
+  , bench "repetition/100" $ whnf (parse (parser (many (char 'a')))) (replicate 100 'a')
+  , bench "repetition/1000" $ whnf (parse (parser (many (char 'a')))) (replicate 1000 'a')
+  ]
