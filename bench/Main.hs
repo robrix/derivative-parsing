9b10bfe773cf9daae971020d447f1d3de4d8c4cb
Rob Rix <rob.rix@github.com>
2016-07-22 01:36:15 -0400

    Group the benchmarks together.

--- bench/Main.hs
+++ bench/Main.hs
@@ -6,8 +6,10 @@ import Derivative.Parser
 
 main :: IO ()
 main = defaultMain
-  [ bench "many/1" $ whnf (parse (parser (many (char 'a')))) (replicate 1 'a')
-  , bench "many/10" $ whnf (parse (parser (many (char 'a')))) (replicate 10 'a')
-  , bench "many/100" $ whnf (parse (parser (many (char 'a')))) (replicate 100 'a')
-  , bench "many/1000" $ whnf (parse (parser (many (char 'a')))) (replicate 1000 'a')
+  [ bgroup "many"
+    [ bench "1" $ whnf (parse (parser (many (char 'a')))) (replicate 1 'a')
+    , bench "10" $ whnf (parse (parser (many (char 'a')))) (replicate 10 'a')
+    , bench "100" $ whnf (parse (parser (many (char 'a')))) (replicate 100 'a')
+    , bench "1000" $ whnf (parse (parser (many (char 'a')))) (replicate 1000 'a')
+    ]
   ]
