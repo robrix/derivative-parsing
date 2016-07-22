419b7ffe381e540774f5e681b2d63cc40ad95954
Rob Rix <rob.rix@github.com>
2016-07-22 01:52:06 -0400

    Compute the benchmarks.

--- bench/Main.hs
+++ bench/Main.hs
@@ -6,11 +6,7 @@ import Derivative.Parser
 
 main :: IO ()
 main = defaultMain
-  [ bgroup "many" $ let p = parser (many (char 'a')) in
-    [ bench "1" $ whnf (parse p) (replicate 1 'a')
-    , bench "10" $ whnf (parse p) (replicate 10 'a')
-    , bench "100" $ whnf (parse p) (replicate 100 'a')
-    , bench "1000" $ whnf (parse p) (replicate 1000 'a')
-    , bench "10000" $ whnf (parse p) (replicate 10000 'a')
-    ]
+  [ bgroup "many" $ let p = parser (many (char 'a'))
+                        b n = bench (show n) $ whnf (parse p) (replicate n 'a') in
+    b . (10 ^) <$> [0..4]
   ]
