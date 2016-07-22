d597d637469415eb339057d7f6a7f105da44a806
Rob Rix <rob.rix@github.com>
2016-07-22 01:35:12 -0400

    Rename the benchmarks.

--- bench/Main.hs
+++ bench/Main.hs
@@ -6,8 +6,8 @@ import Derivative.Parser
 
 main :: IO ()
 main = defaultMain
-  [ bench "repetition/1" $ whnf (parse (parser (many (char 'a')))) (replicate 1 'a')
-  , bench "repetition/10" $ whnf (parse (parser (many (char 'a')))) (replicate 10 'a')
-  , bench "repetition/100" $ whnf (parse (parser (many (char 'a')))) (replicate 100 'a')
-  , bench "repetition/1000" $ whnf (parse (parser (many (char 'a')))) (replicate 1000 'a')
+  [ bench "many/1" $ whnf (parse (parser (many (char 'a')))) (replicate 1 'a')
+  , bench "many/10" $ whnf (parse (parser (many (char 'a')))) (replicate 10 'a')
+  , bench "many/100" $ whnf (parse (parser (many (char 'a')))) (replicate 100 'a')
+  , bench "many/1000" $ whnf (parse (parser (many (char 'a')))) (replicate 1000 'a')
   ]
