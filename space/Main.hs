6712f0018e2a919b001bacf8bf6fb23e624436dd
Rob Rix <rob.rix@github.com>
2016-07-23 11:54:12 -0400

    Stub in a benchmark of a single character parse.

--- space/Main.hs
+++ space/Main.hs
@@ -1,4 +1,9 @@
 module Main where
 
+import Control.Applicative
+import Derivative.Parser
+import Weigh
+
 main :: IO ()
-main = print "TBD"
+main = mainWith $ do
+  func "many/1" (parse (parser (many (char 'a')))) (replicate (10 ^ 0) 'a')
