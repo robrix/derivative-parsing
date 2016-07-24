1de9975ef57693770a1094e49373c4eeb9f0b1c7
Rob Rix <rob.rix@github.com>
2016-07-24 18:25:04 -0400

    Copy most of the ParserF constructors into LexerF.

--- src/Derivative/Lexer.hs
+++ src/Derivative/Lexer.hs
@@ -1,1 +1,18 @@
-module Derivative.Lexer where
+{-# LANGUAGE GADTs #-}
+module Derivative.Lexer
+( LexerF(..)
+) where
+
+data LexerF t f a where
+  Cat :: f a -> f b -> LexerF t f (a, b)
+  Alt :: f a -> f b -> LexerF t f (a, b)
+  Rep :: f a -> LexerF t f [a]
+  Map :: (a -> b) -> f a -> LexerF t f b
+  Bnd :: f a -> (a -> f b) -> LexerF t f b
+  Ret :: [a] -> LexerF t f a
+  Nul :: LexerF t f a
+  Lab :: f a -> String -> LexerF t f a
+  Del :: f a -> LexerF t f a
+
+
+-- Instances
