8bbad182b56c468561055380cbbb48df57328f98
Rob Rix <rob.rix@github.com>
2016-07-24 18:44:39 -0400

    Add (orphaned) Functor, Applicative, Alternative, and Monad instances for Lexer.

--- src/Derivative/Lexer.hs
+++ src/Derivative/Lexer.hs
@@ -1,8 +1,9 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances, GADTs #-}
 module Derivative.Lexer
 ( Lexer
 ) where
 
+import Control.Applicative
 import Data.Higher.Fix
 import Data.Pattern
 
@@ -12,3 +13,20 @@ type Lexer t = Fix (PatternF t)
 
 
 -- Instances
+
+instance Functor (Fix (PatternF t)) where
+  fmap f = Fix . Map f
+
+instance Applicative (Fix (PatternF t)) where
+  pure = Fix . Ret . pure
+  a <*> b = uncurry ($) <$> Fix (Cat a b)
+
+instance Alternative (Fix (PatternF t)) where
+  empty = Fix Nul
+  a <|> b = Fix (Alt a b)
+  some v = (:) <$> v <*> many v
+  many = Fix . Rep
+
+instance Monad (Fix (PatternF t)) where
+  return = pure
+  (>>=) = (Fix .) . Bnd
