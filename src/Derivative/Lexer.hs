2f66424a850a309d7776fc43c90b3e23838448fa
Rob Rix <rob.rix@github.com>
2016-07-24 18:32:10 -0400

    Replace LexerF with PatternF.

--- src/Derivative/Lexer.hs
+++ src/Derivative/Lexer.hs
@@ -3,19 +3,7 @@ module Derivative.Lexer
 ( LexerF(..)
 ) where
 
-import Data.Predicate
-
-data LexerF t f a where
-  Cat :: f a -> f b -> LexerF t f (a, b)
-  Alt :: f a -> f b -> LexerF t f (a, b)
-  Rep :: f a -> LexerF t f [a]
-  Map :: (a -> b) -> f a -> LexerF t f b
-  Bnd :: f a -> (a -> f b) -> LexerF t f b
-  Sat :: Predicate t -> LexerF t f t
-  Ret :: [a] -> LexerF t f a
-  Nul :: LexerF t f a
-  Lab :: f a -> String -> LexerF t f a
-  Del :: f a -> LexerF t f a
+import Data.Pattern
 
 
 -- Instances
