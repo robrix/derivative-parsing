3e297020f11cf616b3247a0a81d9a05b0cebac5b
Rob Rix <rob.rix@github.com>
2016-07-24 18:31:01 -0400

    Copy ParserF in as PatternF.

--- src/Data/Pattern.hs
+++ src/Data/Pattern.hs
@@ -1,3 +1,18 @@
+{-# LANGUAGE GADTs #-}
 module Data.Pattern
-(
+( PatternF(..)
 ) where
+
+import Data.Predicate
+
+data PatternF t f a where
+  Cat :: f a -> f b -> PatternF t f (a, b)
+  Alt :: f a -> f a -> PatternF t f a
+  Rep :: f a -> PatternF t f [a]
+  Map :: (a -> b) -> f a -> PatternF t f b
+  Bnd :: f a -> (a -> f b) -> PatternF t f b
+  Sat :: Predicate t -> PatternF t f t
+  Ret :: [a] -> PatternF t f a
+  Nul :: PatternF t f a
+  Lab :: f a -> String -> PatternF t f a
+  Del :: f a -> PatternF t f a
