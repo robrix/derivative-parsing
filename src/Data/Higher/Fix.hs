49b6c6097950f2b8691c311b706ddd4950ce3e0b
Rob Rix <rob.rix@github.com>
2016-07-24 20:18:04 -0400

    Add an HRecursive instance over higher fixpoints.

--- src/Data/Higher/Fix.hs
+++ src/Data/Higher/Fix.hs
@@ -1,5 +1,16 @@
+{-# LANGUAGE TypeFamilies #-}
 module Data.Higher.Fix
 ( Fix(..)
 ) where
 
+import Data.Higher.Functor
+import Data.Higher.Functor.Recursive
+
 newtype Fix f a = Fix { unFix :: f (Fix f) a }
+
+
+-- Instances
+
+type instance Base (Fix f) = f
+
+instance HFunctor f => HRecursive (Fix f) where hproject = unFix
