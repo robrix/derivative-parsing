7f428a34f751f9c06438acd65ab5e8ca71e76f56
Rob Rix <rob.rix@github.com>
2016-07-24 20:21:30 -0400

    Use natural transformation syntax to declare hproject.

--- src/Data/Higher/Functor/Recursive.hs
+++ src/Data/Higher/Functor/Recursive.hs
@@ -10,7 +10,7 @@ type family Base (t :: k -> *) :: (k -> *) -> k -> *
 -- Classes
 
 class HFunctor (Base t) => HRecursive t where
-  hproject :: t a -> Base t t a
+  hproject :: t ~> Base t t
 
   hcata :: (Base t a ~> a) -> t ~> a
   hcata f = f . hfmap (hcata f) . hproject
