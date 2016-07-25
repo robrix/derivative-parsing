0e0775e3af18ec2cef0d20667a95f734838840b5
Rob Rix <rob.rix@github.com>
2016-07-24 20:14:47 -0400

    Add an HRecursive typeclass.

--- src/Data/Higher/Functor/Recursive.hs
+++ src/Data/Higher/Functor/Recursive.hs
@@ -1,4 +1,9 @@
-{-# LANGUAGE PolyKinds, TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts, PolyKinds, TypeFamilies #-}
 module Data.Higher.Functor.Recursive where
 
-type family Base t :: (k -> *) -> k -> *
+import Data.Higher.Functor
+
+type family Base (t :: k -> *) :: (k -> *) -> k -> *
+
+class HFunctor (Base t) => HRecursive t where
+  hproject :: t a -> Base t t a
