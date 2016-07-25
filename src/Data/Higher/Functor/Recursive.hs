c8b8ca1cd5a12ac79e78e2b93c128c891ba59e82
Rob Rix <rob.rix@github.com>
2016-07-24 20:15:52 -0400

    Add a catamorphism to the typeclass.

--- src/Data/Higher/Functor/Recursive.hs
+++ src/Data/Higher/Functor/Recursive.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE FlexibleContexts, PolyKinds, TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts, PolyKinds, RankNTypes, TypeFamilies, TypeOperators #-}
 module Data.Higher.Functor.Recursive where
 
 import Data.Higher.Functor
+import Data.Higher.Transformation
 
 type family Base (t :: k -> *) :: (k -> *) -> k -> *
 
 class HFunctor (Base t) => HRecursive t where
   hproject :: t a -> Base t t a
+
+  hcata :: (Base t a ~> a) -> t ~> a
