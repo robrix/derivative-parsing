240e65af7b8bea32fd0cda74e392555afa8a94fc
Rob Rix <rob.rix@github.com>
2016-07-31 01:57:28 -0400

    Add an HTraversable typeclass.

--- src/Data/Higher/Traversable.hs
+++ src/Data/Higher/Traversable.hs
@@ -1,1 +1,7 @@
+{-# LANGUAGE RankNTypes #-}
 module Data.Higher.Traversable where
+
+import Data.Higher.Functor
+
+class HFunctor t => HTraversable t where
+  htraverse :: Applicative f => (forall z. a z -> f (b z)) -> t a z -> f (t b z)
