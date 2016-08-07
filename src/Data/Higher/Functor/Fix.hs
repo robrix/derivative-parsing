3fed0c55aabf4283348dbd7cb2fc25a229ae0112
Rob Rix <rob.rix@github.com>
2016-08-07 14:49:23 -0400

    Add catamorphisms over Fix.

--- src/Data/Higher/Functor/Fix.hs
+++ src/Data/Higher/Functor/Fix.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE RankNTypes, TypeOperators #-}
 module Data.Higher.Functor.Fix where
 
+import Data.Higher.Functor
+import Data.Higher.Transformation
+
 newtype Fix f a = Fix { unFix :: f (Fix f) a }
+
+cata :: HFunctor f => (f a ~> a) -> Fix f ~> a
+cata algebra = algebra . hfmap (cata algebra) . unFix
