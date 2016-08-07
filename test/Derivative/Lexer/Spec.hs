8572908e6ac53e7016aa2e70b163a8af0bdf52ab
Rob Rix <rob.rix@github.com>
2016-08-07 14:53:06 -0400

    Test that concatenations pair their inputs.

--- test/Derivative/Lexer/Spec.hs
+++ test/Derivative/Lexer/Spec.hs
@@ -1,7 +1,12 @@
 module Derivative.Lexer.Spec where
 
+import Derivative.Lexer
 import Test.Hspec
+import Test.Hspec.QuickCheck
 
 spec :: Spec
 spec = do
-  pure ()
+  describe "parseNull" $ do
+    describe "cat" $ do
+      prop "returns pairs of its parse trees" $
+        \ a b -> parseNull (pure a `cat` pure b) `shouldBe` [(a, b) :: (Char, Char)]
