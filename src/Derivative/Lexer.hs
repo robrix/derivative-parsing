3bbae9a1ad43a9aa680ab7962a814b1ad9b55e8a
Rob Rix <rob.rix@github.com>
2016-08-07 18:22:48 -0400

    Define the derivative over lexers.

--- src/Derivative/Lexer.hs
+++ src/Derivative/Lexer.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE FlexibleInstances, GADTs #-}
+{-# LANGUAGE FlexibleInstances, GADTs, ScopedTypeVariables #-}
 module Derivative.Lexer
 ( Lexer
+, deriv
 , parseNull
 , compact
 , size
@@ -13,6 +14,7 @@ import Data.Higher.Foldable
 import Data.Higher.Functor.Fix
 import Data.Monoid hiding (Alt)
 import Data.Pattern as Pattern
+import Data.Predicate
 
 -- Types
 
@@ -20,6 +22,19 @@ type Lexer t = Fix (PatternF t)
 
 
 -- Algorithm
+
+deriv :: forall a t. Lexer t a -> t -> Lexer t a
+deriv g c = deriv' g
+  where deriv' :: forall a. Lexer t a -> Lexer t a
+        deriv' (Fix p) = case p of
+          Cat a b -> deriv' a `cat` b <|> delta a `cat` deriv' b
+          Alt a b -> deriv' a <|> deriv' b
+          Rep a -> uncurry (:) <$> (deriv' a `cat` many a)
+          Map f p -> f <$> deriv' p
+          Bnd p f -> deriv' p >>= f
+          Sat p -> if c `satisfies` p then pure c else empty
+          Lab p s -> deriv' p `label` s
+          _ -> empty
 
 parseNull :: Lexer t a -> [a]
 parseNull = cata $ \ parser -> case parser of
