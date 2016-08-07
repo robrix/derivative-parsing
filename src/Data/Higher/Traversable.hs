1bf118b1074ee01a1e6c6a0e902283617c3da041
Rob Rix <rob.rix@github.com>
2016-08-07 11:49:26 -0400

    :fire: Data.Higher.Traversable.

--- src/Data/Higher/Traversable.hs
+++
@@ -1,7 +0,0 @@
-{-# LANGUAGE RankNTypes #-}
-module Data.Higher.Traversable where
-
-import Data.Higher.Functor
-
-class HFunctor t => HTraversable t where
-  htraverse :: Applicative f => (forall z. a z -> f (b z)) -> t a z -> f (t b z)
