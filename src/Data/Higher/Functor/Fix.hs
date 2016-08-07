4086f3f3ee255a2522009995b14c25b27d8aecf4
Rob Rix <rob.rix@github.com>
2016-08-07 16:54:46 -0400

    Add a Base type instance over Fix.

--- src/Data/Higher/Functor/Fix.hs
+++ src/Data/Higher/Functor/Fix.hs
@@ -1,10 +1,16 @@
-{-# LANGUAGE RankNTypes, TypeOperators #-}
+{-# LANGUAGE RankNTypes, TypeFamilies, TypeOperators #-}
 module Data.Higher.Functor.Fix where
 
 import Data.Higher.Functor
+import Data.Higher.Functor.Recursive
 import Data.Higher.Transformation
 
 newtype Fix f a = Fix { unFix :: f (Fix f) a }
 
 cata :: HFunctor f => (f a ~> a) -> Fix f ~> a
 cata algebra = algebra . hfmap (cata algebra) . unFix
+
+
+-- Instances
+
+type instance Base (Fix f) = f
