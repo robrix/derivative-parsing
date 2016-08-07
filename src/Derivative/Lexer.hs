555bdbc57993bbd59980df82d000595086cac5f0
Rob Rix <rob.rix@github.com>
2016-08-07 14:52:37 -0400

    Copy the `parseNull` implementation from Parser.

--- src/Derivative/Lexer.hs
+++ src/Derivative/Lexer.hs
@@ -2,9 +2,11 @@
 module Derivative.Lexer
 ( Lexer
 , cat
+, parseNull
 ) where
 
 import Data.Higher.Functor.Fix
+import Data.Monoid hiding (Alt)
 import Data.Pattern
 
 -- API
@@ -16,3 +18,18 @@ cat a = Fix . Cat a
 -- Types
 
 type Lexer t = Fix (PatternF t)
+
+
+-- Algorithm
+
+parseNull :: Lexer t a -> [a]
+parseNull = cata $ \ parser -> case parser of
+  Cat a b -> (,) <$> a <*> b
+  Alt a b -> a <> b
+  Rep _ -> [[]]
+  Map f p -> f <$> p
+  Bnd p f -> p >>= f
+  Ret as -> as
+  Lab p _ -> p
+  Del a -> a
+  _ -> []
