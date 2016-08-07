73bb666526f97d8b4d01916a7e119a8f7e08159a
Rob Rix <rob.rix@github.com>
2016-08-07 14:42:22 -0400

    Add a `cat` constructor.

--- src/Derivative/Lexer.hs
+++ src/Derivative/Lexer.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE FlexibleInstances, GADTs #-}
 module Derivative.Lexer
 ( Lexer
+, cat
 ) where
 
 import Data.Higher.Functor.Fix
 import Data.Pattern
+
+-- API
+
+cat :: Lexer t a -> Lexer t b -> Lexer t (a, b)
+cat a = Fix . Cat a
+
 
 -- Types
 
