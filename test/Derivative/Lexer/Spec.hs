5c642b22aa4a8bd9fb21e119f1dbcf44c9b8ca20
Rob Rix <rob.rix@github.com>
2016-08-07 14:55:31 -0400

    Test that alternation of lexers behaves as expected.

--- test/Derivative/Lexer/Spec.hs
+++ test/Derivative/Lexer/Spec.hs
@@ -17,3 +17,13 @@ spec = do
 
       prop "is empty when its right operand is empty" $
         \ a -> parseNull (pure a `cat` empty) `shouldBe` ([] :: [(Char, Char)])
+
+    describe "<|>" $ do
+      prop "returns left parse trees" $
+        \ a -> parseNull (pure a <|> empty) `shouldBe` [a :: Char]
+
+      prop "returns right parse trees" $
+        \ b -> parseNull (empty <|> pure b) `shouldBe` [b :: Char]
+
+      prop "returns ambiguous parse trees" $
+        \ a b -> parseNull (pure a <|> pure b) `shouldBe` [a, b :: Char]
